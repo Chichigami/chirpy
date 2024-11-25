f8943f6e88e9f48e4f6bb0f56748742a4ec77ca0
Chichigami <g.feng.work@gmail.com>
2024-11-24 21:23:44 -0500

    metrics added

--- main.go
+++ main.go
@@ -1,22 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
 func main() {
 	const port = "8080"
+	apiCfg := apiConfig{}
 
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.FileSystem(http.Dir(".")))))
+	mux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricInc(http.FileServer(http.FileSystem(http.Dir("."))))))
 	mux.HandleFunc("/healthz", handlerHealthz)
+	mux.HandleFunc("/metrics", apiCfg.handlerMetric)
+	mux.HandleFunc("/reset", apiCfg.handlerMetricReset)
 
 	server := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
-
 	log.Printf("Serving on port: %s\n", port)
 	server.ListenAndServe()
 }
@@ -25,4 +29,27 @@ func handlerHealthz(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(200)
 	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) handlerMetric(w http.ResponseWriter, req *http.Request) {
+	result := fmt.Sprintf("Hits: %d\n", cfg.fileserverHits.Load())
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte(result))
+}
+
+func (cfg *apiConfig) handlerMetricReset(w http.ResponseWriter, req *http.Request) {
+	cfg.fileserverHits.Store(0)
+}
+
+func (cfg *apiConfig) middlewareMetricInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, req)
+	})
+}
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
 }
